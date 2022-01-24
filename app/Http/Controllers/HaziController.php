a56196a82aa1b1d19eca58101af90b5dd6e93e24
Dia <balaska.klaudiaa@gmail.com>
2022-01-24 12:09:25 +0100

    Controller, request

--- app/Http/Controllers/HaziController.php
+++ app/Http/Controllers/HaziController.php
@@ -4,12 +4,53 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 use App\Models\Hazik;
+use App\Http\Requests\HaziRequest;
+use App\Http\Requests\HaziUpdateRequest;
 
 class HaziController extends Controller
 {
+
     public function index()
     {
         $hazik = Hazik::all();
         return response()->json($hazik);
+    }
+
+    public function create()
+    {
+        //
+    }
+
+    public function store(HaziRequest $request)
+    {
+        $h = new Hazik();
+        $h->fill($request->all());
+        $h->save();
+        return response()->json($h, 201);
+    }
+
+    public function show(int $id)
+    {
+        $h = Hazik::findOrFail($id);
+        return response()->json($h);
+    }
+
+    public function edit($id)
+    {
+        //
+    }
+
+    public function update(HaziUpdateRequest $request, int $id)
+    {
+        $h = Hazik::findOrFail($id);
+        $h->fill($request->all());
+        $h->save();
+        return response()->json($h, 200);
+    }
+
+    public function destroy(int $id)
+    {
+        Hazik::destroy($id);
+        return response()->noContent();
     }
 }
